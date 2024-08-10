fb098c1c1b25a6049bbc84ade7f1ab530ff75488
Estar0622 <myjudai@naver.com>
2024-08-11 03:16:56 +0900

    변경

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO article(id, title, content) VALUES(1, '김동규', '삼성전자 DX 사업부');
+INSERT INTO article(id, title, content) VALUES(2, '이솔비', '백석예술대학교');
+INSERT INTO article(id, title, content) VALUES(3, '김동휘', '토스 뱅크 개발부');
+INSERT INTO article(id, title, content) VALUES(4, '유더미', '한성대학교');
